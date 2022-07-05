a2a85d03e75b3ce13ae24689a03d3ebce506ce00
Ar-Dihan <arnahid18@gmail.com>
2022-07-06 00:46:50 +0600

    little lerning but constantly

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ArNahid></ArNahid>
     </div>
   );
+}
+
+function ArNahid(){
+  return (
+    <div style={{color: 'red', forntWeight: 'bold', fontSize: '44px', backgroundColor: 'lightblue', width: '50%', borderRadius: '5px'}}>
+      <p>Atikur Rahman</p>
+      <p>LUT Univarsity</p>
+      <p>MERN DEVELOPER</p>
+    </div>
+  )
 }
 
 export default App;

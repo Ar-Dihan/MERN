07881f24f3921865d736e84bc138ab605552ddb2
Ar-Dihan <arnahid18@gmail.com>
2022-07-08 20:16:30 +0600

    experiment

--- src/App.js
+++ src/App.js
@@ -3,19 +3,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <ArNahid></ArNahid>
+    <ArNahid name="atik" varsity="oulu"></ArNahid>
+    <Cinema name="kutub mainar" category="ancient masjid"></Cinema>
     </div>
   );
 }
 
-function ArNahid(){
+function ArNahid(props){
   return (
-    <div style={{color: 'red', forntWeight: 'bold', fontSize: '44px', backgroundColor: 'lightblue', width: '50%', borderRadius: '5px'}}>
+    <div style={{color: 'red', forntWeight: 'bold', fontSize: '44px', backgroundColor: 'lightblue', borderRadius: '5px'}}>
       <p>Atikur Rahman</p>
-      <p>LUT Univarsity</p>
+      <p>LUT Univarsity and {props.varsity}</p>
+      <h1>{props.name}</h1>
       <p>MERN DEVELOPER</p>
     </div>
-  )
+  );
+}
+
+function Cinema(props) {
+  return (
+    <h1>{props.name} {props.category}</h1> 
+  );
 }
 
 export default App;

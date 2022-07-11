e2a84d24ebf194faa44aeea4b2f89daff1244d8b
Ar-Dihan <arnahid18@gmail.com>
2022-07-11 22:52:16 +0600

    half way done

---
+++ src/components/Product/Product.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Product.css';
+const Product = ({product}) => {
+    const {name, img, seller, price, stock}= product;
+    return (
+        <div className="product">
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div className="productDetail">
+                <h4 className="productName">{name}</h4>
+                <h5>Seller:{seller}</h5>
+                <h5>price:{price}</h5>
+                <p>Only {stock} left in stock: Order soon..</p>
+                <button className="purchaseButton">add to cart</button>
+            </div>
+
+        </div>
+    );
+};
+
+export default Product;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,9 +1,33 @@
-import React from 'react';
-
+import { useEffect,useState } from 'react';
+import Product from '../Product/Product';
+import './Shop.css';
 const Shop = () => {
+
+    const [products, setProducts]= useState([]);
+    useEffect(() =>{
+        fetch('./products.JSON')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            setProducts(data)});
+    }, [])
+
+
+    const handleAddToCart = (product) => {
+            console.log(product);
+    }
     return (
-        <div>
-            <h1>This is shop</h1>
+        <div className="shop-container">
+            <div className="product-container">
+                <h3>Products:</h3>
+                {
+                    products.map(product => <Product product={product} key={product.key} />)
+                }
+            </div>
+            <div className="cart-container">
+                <h3>Order Summary</h3>
+                <h4>Items Ordered: </h4>
+            </div>
         </div>
     );
 };

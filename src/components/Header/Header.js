b8991a22c825ed5996037dcce87fdfd12a260dad
Ar-Dihan <arnahid18@gmail.com>
2022-07-11 12:42:11 +0600

    ema-jhon is just born

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../images/logo.png';
+const Header = () => {
+    return (
+        <div>
+            <h1>This is a Header</h1>
+            <img src={logo} alt=""></img>
+        </div>
+    );
+};
+
+export default Header;

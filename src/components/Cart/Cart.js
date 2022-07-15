ca2e941fec86eeaf6e6787425ebc447c0207c039
Ar-Dihan <arnahid18@gmail.com>
2022-07-15 22:41:35 +0600

    nothing done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import './Cart.css';
 const Cart = (props) => {
-    const {cart}=props;
-    let total=0;
-    for (const product of cart){
-            total=total+product.price;
+    const { cart } = props;
+    let total = 0;
+    for (const product of cart) {
+        total = total + product.price;
     }
     return (
         <div>
             <h3>Order Summary</h3>
             <h4>Items Ordered: {props.cart.length}</h4>
         </div>
-
-        // done nothing but a comment;
     );
 };
 
